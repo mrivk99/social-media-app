bbfcef285d8b54217aa3bf080d96d2bde5f0f789
MRIDUL OJHA <52276607+mrivk99@users.noreply.github.com>
2021-11-27 17:35:04 +0530

    Implementing Basic Drawer Navigation

--- FRONTEND/my-app/src/shared/components/Navigation/MainNavigation.js
+++ FRONTEND/my-app/src/shared/components/Navigation/MainNavigation.js
@@ -1,26 +1,33 @@
 import React from "react";
 
-import './MainNavigation.css'
-import MainHeader from './MainHeader'
+import "./MainNavigation.css";
+import MainHeader from "./MainHeader";
 import { Link } from "react-router-dom";
 import NavLinks from "./NavLinks";
-const MainNavigation = props => {
-
-    return (
-    <MainHeader>
+import SideDrawer from "../Navigation/SideDrawer";
+const MainNavigation = (props) => {
+  return (
+    <React.Fragment>
+      <SideDrawer >
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
         <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
+          <span />
+          <span />
+          <span />
         </button>
         <h1 className="main-navigation__title">
-           <Link to="/"> Your places</Link>
+          <Link to="/"> Your places</Link>
         </h1>
         <nav>
-            <NavLinks/>
+          <NavLinks />
         </nav>
-    </MainHeader>
-    );
-}
+      </MainHeader>
+    </React.Fragment>
+  );
+};
 
-export default MainNavigation
+export default MainNavigation;
