affe745b0f852783a68d7b1cf56cbe7e3ecb5946
MRIDUL OJHA <52276607+mrivk99@users.noreply.github.com>
2021-11-27 17:05:18 +0530

    Adding Navlinks

--- FRONTEND/my-app/src/shared/components/Navigation/MainNavigation.js
+++ FRONTEND/my-app/src/shared/components/Navigation/MainNavigation.js
@@ -3,6 +3,7 @@ import React from "react";
 import './MainNavigation.css'
 import MainHeader from './MainHeader'
 import { Link } from "react-router-dom";
+import NavLinks from "./NavLinks";
 const MainNavigation = props => {
 
     return (
@@ -16,7 +17,7 @@ const MainNavigation = props => {
            <Link to="/"> Your places</Link>
         </h1>
         <nav>
-            ...
+            <NavLinks/>
         </nav>
     </MainHeader>
     );
