e04868fb6941b10204951ce20e466f9db2a7be61
MRIDUL OJHA <52276607+mrivk99@users.noreply.github.com>
2021-11-28 10:49:22 +0530

    Implemented a working SideDrawer with a BackDrop

--- FRONTEND/my-app/src/shared/components/Navigation/MainNavigation.js
+++ FRONTEND/my-app/src/shared/components/Navigation/MainNavigation.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
 import { Link } from "react-router-dom";
 import NavLinks from "./NavLinks";
 import SideDrawer from "../Navigation/SideDrawer";
+import BackDrop from "../UIElements/Backdrop"
+
+
 const MainNavigation = (props) => {
+  const [drawerIsOpen , setDrawerIsOpen] = useState(false);
+
+  const openDrawer = ()=>{
+    setDrawerIsOpen(true);
+  }
+  const closeDrawer = ()=>{
+    setDrawerIsOpen(false);
+  }
   return (
     <React.Fragment>
-      <SideDrawer >
+      {drawerIsOpen ? <BackDrop onClick={closeDrawer}/> : null}
+      {drawerIsOpen ? (<SideDrawer >
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
-      </SideDrawer>
+      </SideDrawer>) : null}
+      
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
           <span />
           <span />
           <span />
