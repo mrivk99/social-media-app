05ff68143d40c2c6e4df9320b28bfd0fde73d1bd
MriOj <mridul.ojha@riaadvisory.com>
2021-12-20 21:49:44 +0530

    Added the User Model

---
+++ BACKEND/models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+// npm install --save mongoose-unique-validator
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+const userSchema = new schema({
+    name: {type: String , required: true},
+    // Unique creates an internal index in the database to make it easier and faster to query our emails.
+    email:{type: String , required: true , unique: true },
+    password: {type: String , required: true , minlength:6 },
+    image:{type: String , required : true},
+    places:{type: Number , required: true}
+});
+
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User',userSchema);
